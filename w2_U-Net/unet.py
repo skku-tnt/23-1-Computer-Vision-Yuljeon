63fa4f91f56b82a0dff5514b677ea76235afbb67
Gloriel621 <gloriel621@gmail.com>
2023-03-13 02:13:21 +0900

    Update unet.py

--- w2_U-Net/unet.py
+++ w2_U-Net/unet.py
@@ -4,79 +4,100 @@ import torch.nn as nn
 
 # performs two convolutional operation 
 # with batch normalization and ReLU activation
-class DoubleConv(nn.Sequential):
-    def __init__(self, in_channels, out_channels):
-        super().__init__(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm2d(out_channels),
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
-# Downsampling is VGG style architecture
-class Down(nn.Sequential):
+    def forward(self, x):
+        return self.double_conv(x)
+
+# VGG style architecture
+class Down(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__(
-            nn.MaxPool2d(kernel_size=2),
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
             DoubleConv(in_channels, out_channels)
         )
 
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
 
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
 
-        self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.conv = DoubleConv(in_channels, out_channels)
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+        
 
     def forward(self, x1, x2):
+        # x1 smaller than x2
         x1 = self.up(x1)
+        
         diffY = x2.size()[2] - x1.size()[2]
         diffX = x2.size()[3] - x1.size()[3]
-        
-        # x1 is smaller than x2. Therefore padding is applyed
-        x1 = nn.functional.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
+
+        x1 = nn.functional.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
         x = torch.cat([x2, x1], dim=1)
         # apply conv to concatenated x1 and x2 
-
         return self.conv(x)
 
 # 1x1 convolution at the end of the model
-class OutConv(nn.Sequential):
+class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__(
-            nn.Conv2d(in_channels, out_channels, kernel_size=1)
-        )
-
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes):
-        super().__init__()
-
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
-        self.down4 = Down(512, 512)
-        self.up1 = Up(1024, 256)
-        self.up2 = Up(512, 128)
-        self.up3 = Up(256, 64)
-        self.up4 = Up(128, 64)
-        self.outc = OutConv(64, n_classes)
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
+        return self.conv(x)
 
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = (DoubleConv(n_channels, 64))
+        self.down1 = (Down(64, 128))
+        self.down2 = (Down(128, 256))
+        self.down3 = (Down(256, 512))
+        factor = 2 if bilinear else 1
+        self.down4 = (Down(512, 1024 // factor))
+        self.up1 = (Up(1024, 512 // factor, bilinear))
+        self.up2 = (Up(512, 256 // factor, bilinear))
+        self.up3 = (Up(256, 128 // factor, bilinear))
+        self.up4 = (Up(128, 64, bilinear))
+        self.outc = (OutConv(64, n_classes))
+
+    def forward(self, x):
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-        # skip connection
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-
-        return self.outc(x)
+        logits = self.outc(x)
+        return logits
 
